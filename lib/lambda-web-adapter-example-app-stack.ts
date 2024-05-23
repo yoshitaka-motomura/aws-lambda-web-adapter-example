b2a2e2179c15e185869c8f8c725414fefad44d4e
Yoshitaka motomura <stay.daybreak@gmail.com>
2024-05-24 02:24:19 +0900

    chore: Update LambdaWebAdapterExampleAppStack to use regional endpoint type for API Gateway domain

--- lib/lambda-web-adapter-example-app-stack.ts
+++ lib/lambda-web-adapter-example-app-stack.ts
@@ -7,12 +7,14 @@ import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as route53 from "aws-cdk-lib/aws-route53";
 import * as route53Targets from "aws-cdk-lib/aws-route53-targets";
 
-//import { HttpApi } from "aws-cdk-lib/aws-apigatewayv2";
-//import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 export class LambdaWebAdapterExampleAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    /**
+     * Create a new Lambda function
+     *　Since we are using a web adapter, this is the only Lambda function we need.
+     */
     const handler = new lambda.DockerImageFunction(
       this,
       "NginxContainerFunction",
@@ -27,38 +29,59 @@ export class LambdaWebAdapterExampleAppStack extends cdk.Stack {
       }
     );
 
-    // rest API
+    /**
+     * Create a new REST API
+     * handler is the lambda function that will be called when the API is called
+     */
     const restApi = new apigateway.LambdaRestApi(this, "NginxRestAPI", {
       handler: handler,
       binaryMediaTypes: ["*/*"],
       restApiName: "OrionWebAdapterAPI",
     });
 
+    /**
+     * Certificate ARN
+     * envronment variable CERTIFICATE_ARN is required
+     */
     const certificateArn = process.env.CERTIFICATE_ARN;
     if (!certificateArn) {
+      // not exist environment variable then throw error
       throw new Error("Certificate ARN is required");
     }
-    //acm
+
+    /**
+     * Get certificate from ACM
+     * certificateArn is the certificate ARN
+     */
     const certificate = acm.Certificate.fromCertificateArn(
       this,
       "Certificate",
       certificateArn
     );
 
-    // domain name
+    /**
+     * apigwDomain is the domain name of the API Gateway
+     */
     const apigwDomain = new apigateway.DomainName(this, "DomainName", {
       domainName: "orion.cristallum.io",
       certificate: certificate,
       endpointType: apigateway.EndpointType.REGIONAL,
     });
 
+    // add base path mapping
     apigwDomain.addBasePathMapping(restApi);
 
-    // route53
+    /**
+     * hostedZone is the hosted zone of the domain
+     */
     const hostedZone = route53.HostedZone.fromLookup(this, "HostedZone", {
       domainName: "cristallum.io",
     });
 
+    /**
+     * AliasRecord is the alias record of the domain
+     * for the API Gateway domain name
+     */
     new route53.ARecord(this, "AliasRecord", {
       zone: hostedZone,
       target: route53.RecordTarget.fromAlias(
